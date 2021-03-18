19b717845b8fe5301ab206f34e3f1c83e77a4541
Basil <vogurt007@gmail.com>
2021-03-18 22:05:28 +0300

    78%

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
+import pathlib
 
 from flask import Flask, request, render_template
 from flask_dropzone import Dropzone
+from PIL import Image
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-#UPLOAD_FOLDER = 'static'
-#ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config.update(
@@ -19,8 +20,34 @@ dropzone=Dropzone(app)
 def upload_file():
     if request.method == 'POST':
         f = request.files.get('file')
-        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename))
+        f.save(os.path.join(app.config['UPLOADED_PATH'], f.filename)) 
     return render_template("index.html")
+
+@app.route('/img/')
+def image_size():
+    path=os.path.join(basedir, 'uploads')
+    dir_list = [os.path.join(path, x) for x in os.listdir(path)]
+    if dir_list:
+        date_list = [[x, os.path.getctime(x)] for x in dir_list]
+    # сортируем список по дате создания в обратном порядке
+        sort_date_list = sorted(date_list, key=lambda x: x[1], reverse=True)
+    try:
+        image = Image.open(sort_date_list[0][0])
+    except FileNotFoundError:  
+        return("Файл не найден")
+    path_file=pathlib.Path(sort_date_list[0][0])
+    name=path_file.name
+    width = image.size[0] # Определяем ширину. 
+    height = image.size[1] # Определяем высоту.  
+    pix = image.load()
+    medium_color = str(pix[4, 4])
+    return render_template(
+        "size.html", 
+        width=width, 
+        height=height, 
+        medium_color=medium_color, 
+        name=name
+        )
 
 
 if __name__ == '__main__':
